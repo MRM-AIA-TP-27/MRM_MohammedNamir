d0182b13ff4e6894fc87ccfdc66133e1c6d413fc
Namir Shah <M-namir-S>
2026-01-06 18:28:04 +0530

    Organizing structure to match remote

---
+++ ROS_tasks/dev_ws/build/chatroom/CMakeFiles/chat_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_node.dir/src/chat_node.cpp.o"
+  "CMakeFiles/chat_node.dir/src/chat_node.cpp.o.d"
+  "chat_node"
+  "chat_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ROS_tasks/dev_ws/build/chatroom/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/namir/GitHub_task/ROS_tasks/dev_ws/src/chatroom
+# Build directory: /home/namir/GitHub_task/ROS_tasks/dev_ws/build/chatroom
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
